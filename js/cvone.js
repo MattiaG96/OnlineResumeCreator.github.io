c6a15ee6db921bda31bb3fbc8886bfbad02a4047
Mattia <40793377+MattiaG96@users.noreply.github.com>
2021-03-27 20:37:57 +0000

    New commit
    New commit

--- js/cvone.js
+++ js/cvone.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
 var basicInfo = []
 var workExperience = []
+var education = []
+var skillList = []
 
 function getBasicInfo() {
 
@@ -60,10 +62,10 @@ function getBasicInfo() {
     */
     if (email === '') {
         document.getElementById("emailContainer").innerHTML = '<span class="badge bg-danger" style="margin: 5px;">Required</span>' +
-            '<input class="form-control" type="text" id="emailInput" placeholder="E-mail" style="border: red 1px solid">';
+            '<input class="form-control" type="email" id="emailInput" placeholder="E-mail" style="border: red 1px solid">';
         return;
     } else {
-        document.getElementById("emailContainer").innerHTML = '<input class="form-control" type="text" id="emailInput" placeholder="E-mail" value="' + email + '">';
+        document.getElementById("emailContainer").innerHTML = '<input class="form-control" type="email" id="emailInput" placeholder="E-mail" value="' + email + '">';
     }
 
     basicInfo.push({
@@ -174,7 +176,7 @@ function populateWorkExperience() {
     var html = '';
 
     workExperience.forEach(work => {
-        html += '<p><h6>' + work.startYear + ' / ' + work.endYear + ' - ' + work.position + ' - ' + work.companyName + ' <button type="button" class="btn" onclick="deleteWork(' + workExperience.indexOf(work) + ')"><i class="far fa-trash-alt"></i></button></h6>' + work.description + '</p>';
+        html += '<p><h6>' + work.startYear + ' / ' + work.endYear + ' - ' + work.position + ' - ' + work.companyName + ' <button type="button" class="btn hiddebleButtons" onclick="deleteWork(' + workExperience.indexOf(work) + ')"><i class="far fa-trash-alt"></i></button></h6>' + work.description + '</p>';
     });
 
     document.getElementById("workExperienceContainer").innerHTML = html;
@@ -184,4 +186,139 @@ function deleteWork(reference) {
     workExperience.splice(reference, 1);
 
     populateWorkExperience();
+}
+
+function saveEducation() {
+    var schoolName = document.getElementById("schoolInput").value.trim();
+    var study = document.getElementById("studyInput").value.trim();
+    var endYear = document.getElementById("studyEndYearInput").value.trim();
+
+    /*
+    Check school
+    */
+    if (schoolName === "") {
+        document.getElementById("schoolNameContainer").innerHTML = '<span class="badge bg-danger" style="margin: 5px;">Required</span>' +
+            '<input class="form-control" type="text" id="schoolInput" placeholder="Name of the school" style="border: red 1px solid">';
+        return;
+    } else {
+        document.getElementById("schoolNameContainer").innerHTML = '<input class="form-control" type="text" id="schoolInput" placeholder="Name of the school" value="' + schoolName + '">';
+    }
+    /*
+    Check study
+    */
+    if (study === "") {
+        document.getElementById("studyContainer").innerHTML = '<span class="badge bg-danger" style="margin: 5px;">Required</span>' +
+            '<input class="form-control" type="text" id="studyInput" placeholder="Study made" style="border: red 1px solid">';
+        return;
+    } else {
+        document.getElementById("studyContainer").innerHTML = '<input class="form-control" type="text" id="studyInput" placeholder="Study made" value="' + study + '">';
+    }
+    /*
+    Check endYear
+    */
+    if (endYear === "") {
+        document.getElementById("studyEndYearContainer").innerHTML = '<span class="badge bg-danger" style="margin: 5px;">Required</span>' +
+            '<input class="form-control" type="text" id="studyEndYearInput" placeholder="End year (2015)" style="border: red 1px solid">';
+        return;
+    } else {
+        document.getElementById("studyEndYearContainer").innerHTML = '<input class="form-control" type="text" id="studyEndYearInput" placeholder="End year (2015)" value="' + endYear + '">';
+    }
+
+    education.push({
+        schoolName: schoolName,
+        study: study,
+        endYear: endYear
+    });
+
+    $('#educationModal').modal('hide');
+    populateEducation();
+
+    document.getElementById("schoolInput").value = '';
+    document.getElementById("studyInput").value = '';
+    document.getElementById("studyEndYearInput").value = '';
+}
+
+function populateEducation() {
+    var html = '';
+
+    education.forEach(study => {
+        html += '<h6 class="h6Edu">' + study.schoolName + ' <button type="button" class="btn hiddebleButtons" onclick="deleteStudy(' + education.indexOf(study) + ')"><i class="far fa-trash-alt"></i></button></h6><p>' + study.study + ' finished in ' + study.endYear + '</p>';
+    });
+
+    document.getElementById("educationContainer").innerHTML = html;
+}
+
+function deleteStudy(studyRef) {
+    education.splice(studyRef, 1);
+
+    populateEducation();
+}
+
+function saveSkill() {
+    var skill = document.getElementById("skillInput").value.trim();
+
+    /*
+    Check skill
+    */
+    if (skill === "") {
+        document.getElementById("skillContainer").innerHTML = '<span class="badge bg-danger" style="margin: 5px;">Required</span>' +
+            '<input class="form-control" type="text" id="skillInput" placeholder="Skill" style="border: red 1px solid">';
+        return;
+    } else {
+        document.getElementById("skillContainer").innerHTML = '<input class="form-control" type="text" id="skillInput" placeholder="Skill" value="' + skill + '">';
+    }
+
+    skillList.push({
+        skill: skill
+    })
+
+    $('#skillModal').modal('hide');
+    document.getElementById("skillInput").value = '';
+    populateSkill();
+}
+
+function populateSkill() {
+    var html = '';
+
+    skillList.forEach(skill => {
+        html += '<h6>- ' + skill.skill + ' <button type="button" class="btn hiddebleButtons" onclick="deleteSkill(' + skillList.indexOf(skill) + ')"><i class="far fa-trash-alt"></i></button></h6>';
+    });
+
+    document.getElementById("skillsContainer").innerHTML = html;
+}
+
+function deleteSkill(skillRef) {
+    skillList.splice(skillRef, 1);
+
+    populateSkill();
+}
+
+function showPDFPreview() {
+    $('.hiddebleButtons').hide();
+    const element = document.getElementById("resumeTemplate");
+    document.getElementById("watermarkTemplate").style.backgroundImage = 'url("../img/watermark.png")'
+    html2pdf().set({
+        pagebreak: {
+            avoid: ['p', 'h6', 'h3']
+        }
+    }).from(element).save('preview.pdf').then(() => {
+        console.log("DONE")
+        $('.hiddebleButtons').show();
+        document.getElementById("watermarkTemplate").style.backgroundImage = 'url("")'
+    });
+
+}
+
+function printPDF() {
+    $('.hiddebleButtons').hide();
+    const element = document.getElementById("resumeTemplate");
+    html2pdf().set({
+        pagebreak: {
+            avoid: ['p', 'h6', 'h3']
+        }
+    }).from(element).save('resume.pdf').then(() => {
+        console.log("DONE")
+        $('.hiddebleButtons').show();
+    });
+
 }
